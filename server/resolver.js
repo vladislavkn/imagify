4e5c14c71448e58de23d75aa8c856505fcd9a291
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-07-11 21:07:45 +0300

    add likes to posts

--- server/resolver.js
+++ server/resolver.js
@@ -3,11 +3,13 @@ const posts = [
     id: "0",
     imageUrl: "https://picsum.photos/200/300",
     text: "test text",
+    likes: 2,
   },
   {
     id: "1",
     imageUrl: "https://picsum.photos/400/400",
     text: "",
+    likes: 1,
   },
 ];
 
@@ -19,8 +21,18 @@ module.exports = {
     return posts.find((post) => post.id === id);
   },
   addPost({ post }) {
-    const newPost = { ...post, id: posts.length.toString() };
+    const newPost = { ...post, id: posts.length.toString(), likes: 0 };
     posts.push(newPost);
     return newPost;
+  },
+  addLike({ id }) {
+    const post = posts.find((post) => post.id === id);
+    post.likes++;
+    return post;
+  },
+  removeLike({ id }) {
+    const post = posts.find((post) => post.id === id);
+    if (post.likes > 0) post.likes--;
+    return post;
   },
 };
