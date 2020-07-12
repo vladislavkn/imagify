2c56546596890478eda743a3d780ff3b98608b4a
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-07-12 14:04:39 +0300

    client: setup router & nav

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <h2 class="header__title">Imagify</h2>
+      <Nav />
+    </header>
+    <router-view />
   </div>
 </template>
+<script>
+import Nav from "@/components/Nav";
 
+export default {
+  components: { Nav },
+};
+</script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: "Inter", Helvetica, Arial, sans-serif;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.header__title {
+  text-align: center;
+  letter-spacing: 0.1rem;
 }
 </style>
---
+++ client/src/components/Nav.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <router-link to="/" class="nav__link" exact-active-class="nav__link_active">
+      Main
+    </router-link>
+    <router-link to="/create" class="nav__link" active-class="nav__link_active">
+      Create
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+};
+</script>
+
+<style>
+nav {
+  display: flex;
+  justify-content: space-evenly;
+  padding: 32px 0;
+}
+
+.nav__link {
+  text-decoration: none;
+  line-height: 1.5rem;
+  color: #000;
+}
+
+.nav__link_active {
+  color: #595cff;
+}
+</style>
